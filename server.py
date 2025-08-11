2c3caf0f1c9bbf8dfc9a1d5127b598fdd3f0f429
darshil929 <darshil929@gmail.com>
2025-08-11 05:37:58 +0530

    fix: update database URI and enhance user creation response

--- server.py
+++ server.py
@@ -11,7 +11,7 @@ from datetime import datetime
 # app
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///sqllitedb.file"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///sqllitedb.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = 0
 
 
@@ -37,3 +37,72 @@ class User(db.Model):
     address = db.Column(db.String(200))
     phone = db.Column(db.String(50))
     posts = db.relationship("BlogPost")
+
+
+class BlogPost(db.Model):
+    __tablename__ = "blog_post"
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50))
+    body = db.Column(db.String(200))
+    date = db.Column(db.Date)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+
+
+@app.route("/user", methods=["POST"])
+def create_user():
+    data = request.get_json()
+    new_user = User(
+        name=data["name"],
+        email=data["email"],
+        address=data["address"],
+        phone=data["phone"],
+    )
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify(
+        {"message": "User created successfully!"}
+    ), 200  # 200 means the request was successful
+
+
+@app.route("/user/descending", methods=["GET"])
+def get_all_users_descending():
+    pass
+
+
+@app.route("/user/ascending", methods=["GET"])
+def get_all_users_ascending():
+    pass
+
+
+@app.route("/user/<user_id>", methods=["GET"])
+def get_one_user(user_id):
+    pass
+
+
+@app.route("/user/<user_id>", methods=["DELETE"])
+def delete_user(user_id):
+    pass
+
+
+@app.route("/blog_post/<user_id>", methods=["POST"])
+def create_blog_post(user_id):
+    pass
+
+
+@app.route("/user/<user_id>", methods=["GET"])
+def get_all_blog_posts(user_id):
+    pass
+
+
+@app.route("/blog_post/<blog_post_id>", methods=["GET"])
+def get_one_blog_post(blog_post_id):
+    pass
+
+
+@app.route("/blog_post/<blog_post_id>", methods=["DELETE"])
+def delete_blog_post(blog_post_id):
+    pass
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
